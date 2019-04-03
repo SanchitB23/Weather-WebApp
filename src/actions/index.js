0f9d09f96d362d959f5a125456c80f8b712ae98c
sanchitb23 <sanbht23@gmail.com>
2019-04-03 14:07:49 +0530

    Updated Packages and Optimized search

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios'; //// NOTE: similar to ajax thingy... will be used to parse like json api
 const API_KEY = ''; //Add your own API Key
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city}`;
   const request = axios.get(url);
     return {
       type: FETCH_WEATHER,

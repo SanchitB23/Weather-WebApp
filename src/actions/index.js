337d98dde5206ab691c60fec61528fcd1d24b881
sanchitb23 <sanbht23@gmail.com>
2019-11-13 18:39:24 +0530

    API related bug fixed, made robust

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,10 +4,14 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?units=metric&a
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
+  if (API_KEY.length == 0) {
+    alert("API key not Set");
+    return;
+  }
   const url = `${ROOT_URL}&q=${city}`;
   const request = axios.get(url);
-    return {
-      type: FETCH_WEATHER,
-      payload: request //request is a promise
-    };
+  return {
+    type: FETCH_WEATHER,
+    payload: request //request is a promise
+  };
 }
